cf41e14c0b51bf74d9d525ceaab34f6a9409386b
marcoossgutierrez <marcos.2006.gs@gmail.com>
2025-01-12 22:26:22 +0100

    Update Nombre.java

--- src/PrimerProyecto_Git/Nombre.java
+++ src/PrimerProyecto_Git/Nombre.java
@@ -3,5 +3,7 @@ package PrimerProyecto_Git;
 public class nombre {
     public static void main(String[] args) {
         System.out.println("Marcos");
+          System.out.println("Esto es el primer error");
+        System.out.println("Esto es el tercer error")
     }
 }
